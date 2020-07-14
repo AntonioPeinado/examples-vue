fce4c18f409c805c70ee44866265abc8b4e58a6a
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-07-14 12:38:02 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+  
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+
+// para iniciar una aplicación de Vue, creamos una nueva instancia
+// y en el método render especificamos qué componente pintar.
+// esa instacia la pintamos en un elemento del DOM con el método $mount
+// que recibe un selector css para referenciar a ese elemento
+const vm = new Vue({
+  render: h => h(App),
+})
+
+vm.$mount('#app');
