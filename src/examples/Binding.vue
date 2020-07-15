b21fed8defb6798a99aae952d3a4e7805aff8f7f
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-07-15 09:49:55 +0200

    1

--- src/examples/Binding.vue
+++ src/examples/Binding.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h2>Binding</h2>
-    <!-- attribute binding (v-bind === :) -->
+   
     <button :disabled="true" :foo="1">Disabled</button>
-    <!-- event binding (v-on === @) -->
+   
     <button @click="onClick">Click</button>
   </div>
 
@@ -12,7 +12,7 @@
 <script>
 export default {
   name: "Binding",
-  // define los métodos del componente
+  
   methods: {
     onClick(ev){
       console.log(ev);
