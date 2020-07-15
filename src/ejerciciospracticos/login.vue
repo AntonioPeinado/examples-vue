6f5136ea4cd75dcb163647b745cd9dd11c2bd796
Antonio Peinado <60774350+AntonioPeinado@users.noreply.github.com>
2020-07-15 17:06:06 +0200

    quitar fallos

--- src/ejerciciospracticos/login.vue
+++ src/ejerciciospracticos/login.vue
@@ -5,9 +5,15 @@
       <input :id="emailId" name="email" @blur="validateEmail" v-model="email" type="text" />
       <p role="alert" v-if="emailError">{{emailError}}</p>
     </div>
-     <div>
-      <label :for="passworId">Password:</label>
-      <input :id="passworId" name="password" @blur="validatePassword" v-model="password" type="text" />
+    <div>
+      <label :for="passwordId">Password:</label>
+      <input
+        :id="passwordId"
+        name="password"
+        @blur="validatePassword"
+        v-model="password"
+        type="text"
+      />
       <p role="alert" v-if="passwordError">{{passwordError}}</p>
     </div>
     <!-- <FormField name="email" id="email" label="Email:" v-model="email" type="text" validators="[isEmail]" >-->
@@ -15,50 +21,58 @@
   </form>
 </template>
 <script>
-import uuid from 'uuid';
-const isEmail = (value, msg = 'Formato de email incorrecto') => {
-    const isValidEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value);
-    return isValidEmail ? '' : msg;
-} 
-const isPassword = (value, msg = 'Formato de password incorrecto') => {
-    const isValidPassword = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/.test(value);
-    return isValidPassword ? '' : msg;
-}
+import uuid from "uuid";
+const isEmail = (value, msg = "Formato de email incorrecto") => {
+  const isValidEmail = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+    value
+  );
+  return isValidEmail ? "" : msg;
+};
+const isPassword = (value, msg = "Formato de password incorrecto") => {
+  const isValidPassword = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/.test(
+    value
+  );
+  return isValidPassword ? "" : msg;
+};
 export default {
-    data() {
-        return {
-            emailId: uuid,
-            passwordId: uuid,
-            email: '',
-            password: '',
-            emailError: '',
-            passwordError: ''
-        }
+  data() {
+    return {
+      emailId: uuid,
+      passwordId: uuid,
+      email: "",
+      password: "",
+      emailError: "",
+      passwordError: ""
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.validateForm();
+      if (this.isFormValid()) {
+        return console.log({ email: this.email, password: this.password });
+      }
     },
-    methods: {
-        onSubmit(){
-            this.validateForm();
-            if(this.isFormValid()){
-             return   console.log({email: this.email, password: this.password})
-            }
-        },
-        validateForm(){
-            this.validateEmail();
-        },
-        isFormValid(){
-            return !this.emailError && !this.passwordError;
-        },
-        validateEmail(){
-            this.emailError = isEmail(this.email, 'formato del email incorrecto');
-        },
-        validatePassword(){
-            this.passwordError = isPassword(this.password, 'formato del password incorrecto');
-        }
+    validateForm() {
+      this.validateEmail();
+      this.validatePassword()
+    },
+    isFormValid() {
+      return !this.emailError && !this.passwordError;
+    },
+    validateEmail() {
+      this.emailError = isEmail(this.email, "formato del email incorrecto");
+    },
+    validatePassword() {
+      this.passwordError = isPassword(
+        this.password,
+        "formato del password incorrecto"
+      );
     }
-}
+  }
+};
 </script>
 <style scoped>
-    h2 {
-        color:darkblue;
-    }
+h2 {
+  color: darkblue;
+}
 </style>
